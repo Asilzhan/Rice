fa9fce40823766fd0cdc0cf0f57b20be728f6bd4
zhanseit.a <zhanseit.a@hcsbk.kz>
2023-08-16 17:00:00 +0600

    Initial commit

---
+++ PrimeBlazor.Components/Button/ButtonPassThroughOptions.cs
@@ -0,0 +1,27 @@
+﻿using OneOf;
+
+namespace PrimeBlazor.Components.Button;
+
+public class ButtonPassthroughOptions
+{
+    public PassThroughOption? Root { get; set; }
+    public string? LoadingIcon { get; set; }
+    public string? Icon { get; set; }
+    public string? Label { get; set; }
+    public string? Badge { get; set; }
+}
+
+public class PassThroughOption : OneOfBase<string, Func<ButtonPtContext, string>>
+{
+    private PassThroughOption(OneOf<string, Func<ButtonPtContext, string>> _) : base(_) { }
+    
+    public static implicit operator PassThroughOption(string _) => new(_);
+    public static implicit operator PassThroughOption(Func<ButtonPtContext, string> _) => new(_);
+    
+    public static PassThroughOption FromFunc(Func<ButtonPtContext, string> func) => func;
+}
+
+public class ButtonPtContext
+{
+    public object? Instance { get; set; }
+}
